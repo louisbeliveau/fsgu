a0dcd774915eaa342681a633a6129b512b177ac9
louis911911 <louis911911@yahoo.com>
2023-02-04 02:08:11 -0500

    initial

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let sayHello person=
+    printfn "Hello %s from F#" person
+
+let isValid person=
+    not(String.IsNullOrWhiteSpace(person))  
+
+let isAllowed person=
+    person<>"Eve"
+    
+
+// For more information see https://aka.ms/fsharp-console-apps
+[<EntryPoint>]
+let main argv =
+    argv
+    |>Array.filter isValid
+    |>Array.filter isAllowed
+    |>Array.iter sayHello        
+    printfn "nice to meet you"
+    0
